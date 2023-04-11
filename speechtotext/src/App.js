d6f4baf6232ee38467969f7fe3555b261d7d1d61
callmejkode <callmejkode@gmail.com>
2023-04-11 12:11:45 +0530

    patch work by remove the alert

--- speechtotext/src/App.js
+++ speechtotext/src/App.js
@@ -4,14 +4,14 @@ import SpeechRecognition, {
 } from "react-speech-recognition";
 
 export default function App() {
-  const inputRef = useRef(null);
+  const inputRef = useRef();
 
   async function copythetext() {
     try {
       await navigator.clipboard.writeText(inputRef.current.value);
-      alert("Text copied to clipboard!");
     } catch (err) {
       console.error("Failed to copy text: ", err);
+      alert("Failed to copy text");
     }
   }
 
@@ -27,6 +27,43 @@ export default function App() {
 
   return (
     <>
+      <div
+        class="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title text-black" id="staticBackdropLabel">
+                Text Copied!
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body text-black">
+              Thanks for using my application :D{" "}
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="container-fluid">
         <div className="container text-center mainBody ">
           <h1 className="pt-3 text-center">Speech to Text Converter</h1>
@@ -38,7 +75,12 @@ export default function App() {
               value={transcript}
             ></textarea>
             <div className="d-lg-flex justify-content-around py-4">
-              <button className="my-2 mx-2" onClick={copythetext}>
+              <button
+                className="my-2 mx-2"
+                onClick={copythetext}
+                data-bs-toggle="modal"
+                data-bs-target="#staticBackdrop"
+              >
                 Copy text
               </button>
               <button className="my-2 mx-2" onClick={startListening}>
